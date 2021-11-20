252a2a6c97b2e5191bb70ee560eb4b47b2f2e14f
hatemsallam <hatemsallam94@hotmail.com>
2021-11-20 20:09:34 +0200

    finish lab38

--- app/src/main/java/com/example/taskmaster/SignIn.java
+++ app/src/main/java/com/example/taskmaster/SignIn.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -22,11 +25,30 @@ public class SignIn extends AppCompatActivity {
     private static final String TAG = "SignIn";
     Handler handler;
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(PushListenerService.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         configureAmplify();
+
+        createNotificationChannel();
+
         Button signUpButton = findViewById(R.id.SignUpbuttonsignInpage);
         Button signInButton = findViewById(R.id.signInbuttonid);
         Toast toast = Toast.makeText(this, "Invalid Email or Password , Try Again", Toast.LENGTH_LONG);
